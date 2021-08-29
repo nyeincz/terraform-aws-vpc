cf5fc48ffa3665ccec7e87863518820afc6dfd41
Chan XPS <nyeincz@Gmail.com>
2021-08-29 15:21:04 +0800

    create aws vpc with terraform moudles

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.region
+}
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "terraform_vpc"
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = "10.10.1.0/24"
+}
